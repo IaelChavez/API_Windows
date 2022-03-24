57b720e2dd85ea32e02f83b01e7a67fcda2adab0
ZBooK <ZBooK@DESKTOP-GRUJMHB>
2022-03-24 15:33:05 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using API_Windows.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_Windows.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET: <ProductController>
+        [HttpGet]
+        public List<ProductModel> GetAll()
+        {
+            return new ProductModel().GetAll();
+            
+        }
+
+        // GET <ProductController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"Hola, el id envieado es {id}";
+
+        }
+
+        // POST <ProductController>
+        [HttpPost]
+        public void Post([FromBody] ProductModel model)
+        {
+            new ProductModel().Add(model);
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut]
+        public void Put([FromBody] ProductModel model)
+        {
+            new ProductModel().Update(model);
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            new ProductModel().Delete(id);
+        }
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,67 @@
+﻿namespace API_Windows.Models
+{
+    public class ProductModel
+    {
+        // Variables locales
+        private static List<ProductModel> Products = new List<ProductModel>();
+
+        public int ID { get; set; }
+        public string Model { get; set; }
+        public string Category { get; set; }
+        public float Price { get; set; }
+        public string Picture { get; set; }
+        public string? Description { get; set; }
+        public string Designer { get; set; }
+
+        public ProductModel()
+        {
+            Model = String.Empty;
+            Category = String.Empty;
+            Picture = String.Empty; 
+            Designer = String.Empty;
+
+            //Products = new List<ProductModel>();
+        }
+
+        //Funciones CRUD
+
+        public ProductModel Get(int id)
+        {
+            return Products.Find(p => p.ID == id);
+        }
+
+        public List<ProductModel> GetAll()
+        {
+            return Products;
+        }
+
+        public int Add(ProductModel model)
+        {
+            model.ID = Products.Count + 1;
+            Products.Add(model);
+            return model.ID;
+        }
+
+        public void Update(ProductModel model)
+        {
+            foreach(ProductModel item in Products)
+            {
+                if(item.ID == model.ID)
+                {
+                    item.Price = model.Price;
+                    item.Picture = model.Picture;
+                    item.Description = model.Description;
+                    item.Designer = model.Designer;
+                    item.Category = model.Category;
+                    item.Model = model.Model;
+                    break;
+                }
+            }
+        }
+        public void Delete(int id)
+        {
+            Products.Remove(Get(id));
+        }
+    }
+}
+
