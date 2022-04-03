9036871342e6697b5910b9b9b4e66e3fe817c8be
IaelChavez <jcs72484@udelasalle.edu.mx>
2022-04-03 18:50:49 -0500

    Base de datos en Api lista

--- Models/ProductModel.cs
+++ Models/ProductModel.cs
@@ -1,17 +1,23 @@
-﻿namespace API_Windows.Models
+﻿using MySql.Data.MySqlClient;
+
+namespace API_Windows.Models
 {
     public class ProductModel
     {
         // Variables locales
-        private static List<ProductModel> Products = new List<ProductModel>();
+
+        private string ConnectionString = "Server=localhost; Port=3306; Database=amazon_seler; User ID=root; Password=Conchudo1";
+        
+        //Memoria
+        //private static List<ProductModel> Products = new List<ProductModel>();
 
         public int ID { get; set; }
-        public string Model { get; set; }
-        public string Category { get; set; }
+        public string? Model { get; set; }
+        public string? Category { get; set; }
         public float Price { get; set; }
-        public string Picture { get; set; }
+        public string? Picture { get; set; }
         public string? Description { get; set; }
-        public string Designer { get; set; }
+        public string? Designer { get; set; }
 
         public ProductModel()
         {
@@ -27,24 +33,162 @@
 
         public ProductModel Get(int id)
         {
-            return Products.Find(p => p.ID == id);
+            //Memoria
+            //return Products.Find(p => p.ID == id);
+
+            //MySQL
+            ProductModel model = new ProductModel();
+            try
+            {
+                using (MySqlConnection con = new MySqlConnection(ConnectionString))
+                {
+                    con.Open();
+                    string tsql = "SELECT * FROM product WHERE IDproduct = @ID";
+                    using (MySqlCommand cmd = new MySqlCommand(tsql, con))
+                    {
+                        cmd.Parameters.AddWithValue("@ID", id);
+
+                        //Ejecutar la consulta
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                model = new ProductModel()
+                                {
+                                    ID = int.Parse(reader["IDproduct"].ToString()),
+                                    Model = reader["Model"].ToString(),
+                                    Category = reader["Category"].ToString(),
+                                    Price = float.Parse(reader["Price"].ToString()),
+                                    Description = reader["Description"].ToString(),
+                                    Picture = reader["Picture"].ToString(),
+                                    Designer = reader["Designer"].ToString()
+                                };
+                             
+                            }
+                        }
+                    }
+                }
+                //Devolvemos el modelo
+                return model;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public List<ProductModel> GetAll()
         {
-            return Products;
+            //Memoria
+            //return Products;
+
+            //MySQL
+
+            List<ProductModel> col = new List<ProductModel>();
+            try
+            {
+                using (MySqlConnection con = new MySqlConnection(ConnectionString))
+                {
+                    con.Open();
+                    string tsql = "SELECT * FROM product";
+                    using (MySqlCommand cmd = new MySqlCommand(tsql, con))
+                    {
+                        //Ejecutar la consulta
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                col.Add(new ProductModel()
+                                {
+                                    ID = int.Parse(reader["IDproduct"].ToString()),
+                                    Model = reader["Model"].ToString(),
+                                    Category = reader["Category"].ToString(),
+                                    Price = float.Parse(reader["Price"].ToString()),
+                                    Description = reader["Description"].ToString(),
+                                    Picture = reader["Picture"].ToString(),
+                                    Designer = reader["Designer"].ToString()
+                                });
+
+                            }
+                        }
+                    }
+                }
+                //Devolvemos una coleccion
+                return col;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public int Add(ProductModel model)
         {
+            //Memoria
+
+            /*
             model.ID = Products.Count + 1;
             Products.Add(model);
-            return model.ID;
+            return model.ID;*/
+
+            //MySQL
+
+            try
+            {
+                object newID;
+                using(MySqlConnection con = new MySqlConnection(ConnectionString))
+                {
+                    con.Open();
+                    string tsql = "INSERT INTO product " +
+                                  "(Model, " +
+                                  "Category, " +
+                                  "Price, " +
+                                  "Picture, " +
+                                  "Description, " +
+                                  "Designer) " +
+                                  "VALUES " +
+                                  "(@Model, " +
+                                  "@Category, " +
+                                  "@Price, " +
+                                  "@Picture, " +
+                                  "@Description, " +
+                                  "@Designer);" +
+                                  "SELECT LAST_INSERT_ID();";
+                    using(MySqlCommand cmd = new MySqlCommand(tsql, con))
+                    {
+                        cmd.CommandType = System.Data.CommandType.Text;
+                        cmd.Parameters.AddWithValue("@Model", model.Model);
+                        cmd.Parameters.AddWithValue("@Category", model.Category);
+                        cmd.Parameters.AddWithValue("@Price", model.Price);
+                        cmd.Parameters.AddWithValue("@Picture", model.Picture);
+                        cmd.Parameters.AddWithValue("@Description", model.Description);
+                        cmd.Parameters.AddWithValue("@Designer", model.Designer);
+
+                        newID = cmd.ExecuteScalar();//Nos debe de devolver el nuevo ID
+
+                        if(newID != null && newID.ToString().Length > 0)
+                        {
+                            return int.Parse(newID.ToString());
+                        }
+                    }
+                }
+                return 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public void Update(ProductModel model)
         {
-            foreach(ProductModel item in Products)
+            //Memoria
+            /*foreach(ProductModel item in Products)
             {
                 if(item.ID == model.ID)
                 {
@@ -56,11 +200,71 @@
                     item.Model = model.Model;
                     break;
                 }
+            }*/
+
+            //MySQL
+            try
+            {
+             
+                using (MySqlConnection con = new MySqlConnection(ConnectionString))
+                {
+                    con.Open();
+                    string tsql = "UPDATE product SET Model = @Model, Category = @Category, Price = @Price, Picture = @Picture, Description = @Description, Designer = @Designer " +
+                                  "WHERE IDproduct = @IDproduct";
+
+                    using (MySqlCommand cmd = new MySqlCommand(tsql, con))
+                    {
+                        cmd.CommandType = System.Data.CommandType.Text;
+                        cmd.Parameters.AddWithValue("@Model", model.Model);
+                        cmd.Parameters.AddWithValue("@Category", model.Category);
+                        cmd.Parameters.AddWithValue("@Price", model.Price);
+                        cmd.Parameters.AddWithValue("@Picture", model.Picture);
+                        cmd.Parameters.AddWithValue("@Description", model.Description);
+                        cmd.Parameters.AddWithValue("@Designer", model.Designer);
+                        cmd.Parameters.AddWithValue("@IDproduct", model.ID);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                
             }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
         }
         public void Delete(int id)
         {
-            Products.Remove(Get(id));
+            //Memoria
+            //Products.Remove(Get(id));
+
+            //MySQL
+
+            try
+            {
+
+                using (MySqlConnection con = new MySqlConnection(ConnectionString))
+                {
+                    con.Open();
+                    string tsql = "DELETE FROM product WHERE IDproduct = @IDproduct";
+
+                    using (MySqlCommand cmd = new MySqlCommand(tsql, con))
+                    {
+                        cmd.CommandType = System.Data.CommandType.Text;
+                        cmd.Parameters.AddWithValue("@IDproduct", id);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
         }
     }
 }
